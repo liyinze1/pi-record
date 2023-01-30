bdd964fe4b34de0db0b719d00b5237df1878c630
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-01-30 22:49:18 +0100

    update cmd

--- utils.py
+++ utils.py
@@ -1,16 +1,17 @@
 import subprocess
 import time
+import shlex
 
 class vpn:
     
     def __init__(self):
         self.vpn_thread = None
-        self.command = 'sudo openvpn --config /home/pi/audio-pi-1.ovpn'
+        self.args = shlex.split('sudo openvpn --config /home/pi/audio-pi-1.ovpn')
         
     def connect(self):
         if self.check():
             return 'It\'s already connected'
-        self.vpn_thread = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.vpn_thread = subprocess.Popen(self.args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if self.check():
             return 'connected!'
         else:
@@ -26,12 +27,12 @@ class record:
     
     def __init__(self):
         self.record_thread = None
-        self.command = '/usr/bin/arecord -Dac108 -f S32_LE -r 48000 -c 4 | /usr/bin/ffmpeg -re -i - -acodec pcm_s24be -f rtp rtp://10.94.0.31:23000 -sdp_file /home/pi/24.sdp'
+        self.args = shlex.split('/usr/bin/arecord -Dac108 -f S32_LE -r 48000 -c 4 | /usr/bin/ffmpeg -re -i - -acodec pcm_s24be -f rtp rtp://10.94.0.31:23000 -sdp_file /home/pi/24.sdp')
     
     def record(self):
         if self.record_thread is not None and self.record_thread.poll() is None:
             return 'it\'s already recording'
-        self.record_thread = subprocess.Popen(self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.record_thread = subprocess.Popen(self.args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(2)
         if self.record_thread.poll() is None:
             return 'recording...'
