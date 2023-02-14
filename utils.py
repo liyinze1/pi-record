bd1bab2413a996214e569a1f2192dcf40b56d0bc
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-02-14 11:54:32 +0100

    update

--- utils.py
+++ utils.py
@@ -2,6 +2,9 @@ import subprocess
 import time
 import shlex
 
+server_ip = '10.94.0.31'
+vpn_network = '10.94.0'
+
 class vpn:
     
     def __init__(self):
@@ -22,7 +25,7 @@ class vpn:
     
     def check(self):
         process = subprocess.run(["ifconfig"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8", timeout=1)
-        return '10.94.0.' in process.stdout
+        return vpn_network in process.stdout
         
 
 class record:
@@ -31,7 +34,7 @@ class record:
         self.record_thread = None
         self.stream_thread = None
         self.record_args = shlex.split('/usr/bin/arecord -Dac108 -f S32_LE -r 48000 -c 4')
-        self.stream_args = shlex.split('/usr/bin/ffmpeg -re -i - -acodec pcm_s24be -f rtp rtp://10.94.0.31:23000 -sdp_file /home/pi/24.sdp')
+        self.stream_args = shlex.split('/usr/bin/ffmpeg -re -i - -acodec pcm_s24be -f rtp rtp://%s:23000 -sdp_file /home/pi/24.sdp'%server_ip)
     
     def record(self):
         if self.record_thread is not None and self.record_thread.poll() is None:
