886361335bf69c8c2c76153794061758247f4541
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2023-07-03 11:08:00 +0200

    1

--- setup-server.sh
+++ setup-server.sh
@@ -1,6 +1,6 @@
 sudo apt install -y ffmpeg
 sudo apt install -y python3-pip
-pip3 install -r requirements/pi/requirements.txt
+pip3 install -r requirements/server/requirements.txt
 sudo cp server-record.service /etc/systemd/system/
 sudo systemctl enable server-record.service
 sudo systemctl start server-record.service
