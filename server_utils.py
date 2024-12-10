3e5f187dc98435c37042dd0d7d1c60f34d417a96
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-12-10 21:46:16 +0100

    debug logger

--- server_utils.py
+++ server_utils.py
@@ -25,9 +25,9 @@ class Port_controller:
         self.port_list = [i for i in range(23000, 23010, 2)]
 
     def get_port(self):
-        logger.info("number of ports", len(self.port_list))
+        logger.info("number of ports %d" % len(self.port_list))
         for port in self.port_list:
-            logger.info("checking", port)
+            logger.info("checking %d" % port)
             if self.check_port(port):
                 self.port_list.remove(port)
                 return port
@@ -44,7 +44,7 @@ class Port_controller:
             sock.bind(('0.0.0.0', port))
             result = True
         except Exception as e:
-            logger.info("Port is in use", e)
+            logger.info("Port is in use: %d" % e)
         sock.close()
         return result
 
