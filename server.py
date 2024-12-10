77b49613cc5a52928d6673847663f19f9518c2ef
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2024-12-10 21:02:54 +0100

    update server ip

--- server.py
+++ server.py
@@ -64,5 +64,5 @@ def label():
     return 'ok'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=443, use_reloader=False, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='10.94.0.16', debug=True, port=443, use_reloader=False, ssl_context=('cert.pem', 'key.pem'))
     # app.run(host='127.0.0.1', debug=True, port=8000)
