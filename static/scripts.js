3f3083ddfaaf8dce90d5ca2bae2f4cb084ac5f5a
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-02-18 15:32:47 +0100

    window.open work

--- static/scripts.js
+++ static/scripts.js
@@ -244,6 +244,8 @@ function select_protocol(option) {
 
 // Check Audios
 function fetch_audio() {
+    document.getElementById('audio_source').src = '';
+    document.getElementById('audio_box').load();
     if (!check_vin()) {
         return;
     }
@@ -298,27 +300,6 @@ function download() {
         return;
     }
     window.open('/play/' + audio_name)
-    // fetch('/play/' + audio_name)
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error(`Network response was not OK (${response.status})`);
-    //         }
-    //         return response.blob();  // Convert the response to a Blob
-    //     })
-    //     .then(blob => {
-    //         const url = URL.createObjectURL(blob);  // Create a temporary URL
-    //         const link = document.createElement('a');
-    //         link.href = url;
-    //         link.download = audio_name;  // Set the downloaded filename
-    //         document.body.appendChild(link);
-    //         link.click();  // Trigger the download
-    //         document.body.removeChild(link);
-    //         URL.revokeObjectURL(url);  // Clean up
-    //     })
-    //     .catch(error => {
-    //         console.error('Download failed:', error);
-    //         alert('Failed to download the file.');
-    //     });
 }
 
 function delete_audio() {
