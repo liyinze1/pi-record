b544aa4fc2ab445875ec76e14815a7b585c65b3d
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-03-26 16:01:35 +0100

    update server

--- server.py
+++ server.py
@@ -75,10 +75,10 @@ def play(audio):
 @app.route('/delete/<audio>', methods=['GET'])
 def delete(audio):
     # try:
-    #     os.remove(os.path.join(audio_folder, audio))
+    os.remove(os.path.join(audio_folder, audio))
     # except OSError as e:
     #     print(f'Error deleting {audio}: {e}')
-    print('deleting', audio, 'cmd = ', 'rm -f ' + os.path.join(audio_folder, audio))
+    # print('deleting', audio, 'cmd = ', 'rm -f ' + os.path.join(audio_folder, audio))
     # os.system('rm -f ' + os.path.join(audio_folder, audio))
     
     return 'deleted'
