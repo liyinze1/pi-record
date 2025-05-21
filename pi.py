f185c60c10df83416139347336106334d2bb7d7b
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-05-21 16:58:53 +0200

    update

--- pi.py
+++ pi.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request
 from pi_utils import *
 
-app = Flask(__name__)
-
 pi_recorder = Pi_recorder()
 
+app = Flask(__name__)
 
 @app.route('/status', methods=['GET'])
 def status():
