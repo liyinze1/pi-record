60f273f918ebec6c1e945ea8ea0e68a815e21b56
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-05-21 17:09:26 +0200

    rest after update to eink

--- pi_utils.py
+++ pi_utils.py
@@ -39,12 +39,14 @@ class Ink_screen_controller:
         msg = 'M' + msg + datetime.now().strftime(' updated_at:%Y-%m-%d-%H:%M:%S')
         print('writing to E-INK:', msg)
         self.ser.write(msg.encode('ascii'))
+        time.sleep(0.2)
         
     def update_token(self, token):
         """Update the ink screen with the given token."""
         msg = 'T' + token
         print('writing to E-INK:', msg)
         self.ser.write(msg.encode('ascii'))
+        time.sleep(0.2)
 
 class ATCommandInterface:
     def __init__(self, port='/dev/ttyUSB2', baudrate=115200, timeout=1):
