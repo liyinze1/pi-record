cedcd8776b7f1cad66b24040d1af5774aadec911
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-06-17 20:58:48 +0200

    update server address

--- server_utils.py
+++ server_utils.py
@@ -94,7 +94,7 @@ class Receive:
             # thread for receiving
             cmd = 'ffmpeg -protocol_whitelist file,http,rtp,tcp,udp -i %s -acodec pcm_s24le %s' % (self.sdp_filename, self.audio_filename)
         else:
-            cmd = 'timeout 180 ffmpeg -f s32le -ac 2 -ar 48000 -i \'tcp://0.0.0.0:%d?listen=1\' -acodec copy %s' % (port, self.audio_filename)
+            cmd = 'timeout 180 ffmpeg -f s32le -ac 2 -ar 48000 -i tcp://0.0.0.0:%d?listen=1 -acodec copy %s' % (port, self.audio_filename)
         
         print(cmd)
         cmd = shlex.split(cmd)
