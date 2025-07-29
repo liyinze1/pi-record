4cc926734867e30caa27720c2308d019acaa815b
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-07-29 23:25:43 +0200

    debug frequence

--- audio_check.py
+++ audio_check.py
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     vin_set = load_labels()
     
     target_length = 50
-    target_rate = 22500
+    target_rate = 22050
     target_dir = './data'
     
     target_label = {}
