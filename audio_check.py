0e0b9586f8c700320b6b2b368b55af5a834a7e30
aliyun0442448136 <50014026+liyinze1@users.noreply.github.com>
2025-09-11 16:36:48 +0200

    update train test set

--- audio_check.py
+++ audio_check.py
@@ -198,11 +198,11 @@ if __name__ == '__main__':
             sf.write(target_path, data, rate, 'PCM_32')
             
             # VIN,recording_setup,date,audio_file,annotations_file,segmentation_file,manual_annotation,anomaly,no-noise,split
-            # if random.random() < 0.5:
+            if random.random() < 0.2:
             # if vin_set[vin] == 0:
-            f.write(f'{vin},pi,{date},{filename},annotaion.csv,,FALSE,{vin_set[vin] != 0},FALSE,te\n')
-            # else:
-            f.write(f'{vin},pi,{date},{filename},annotaion.csv,,FALSE,{vin_set[vin] != 0},FALSE,tr\n')
+                f.write(f'{vin},pi,{date},{filename},annotaion.csv,,FALSE,{vin_set[vin] != 0},FALSE,te\n')
+            else:
+                f.write(f'{vin},pi,{date},{filename},annotaion.csv,,FALSE,{vin_set[vin] != 0},FALSE,tr\n')
             
             target_label[vin] = vin_set[vin]
             
